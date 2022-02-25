a5e51e351e1d9f6f5f3c1953f6bcd00f97729597
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-02-25 19:56:37 +0200

    removed warning

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
-import { RoomConsumer, withRoomConsumer } from "../context";
+import { withRoomConsumer } from "../context";
 import Loading from "./Loading";
 
 function RoomContainer({ context }) {
