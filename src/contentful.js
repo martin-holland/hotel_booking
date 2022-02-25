d5ce390768335aaaf6c220995b569821f1aecfe5
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-02-25 19:52:17 +0200

    environment change

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "g828c8ohyx7y",
-  accessToken: "QfTCruNRcknINBx9fNdGS6yT5ZO_VlgwuBGP0LTerpo",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
